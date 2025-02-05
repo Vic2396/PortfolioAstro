1e62fe1507dfc2fe46cca74fd9d7664dad59c091
victor mora rosique <vic23mora@gmail.com>
2025-02-05 15:15:14 +0100

    Funcionalidad y responsive de componente header y página inicio

--- src/scripts/desplegableMenu.js
+++ src/scripts/desplegableMenu.js
@@ -6,9 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
       if (menu?.classList.contains("max-h-0")) {
         menu.classList.remove("max-h-0");
         menu.classList.add("max-h-[200px]"); 
+        toggleButton.classList.add("rotate-180");
       } else {
         menu?.classList.remove("max-h-[200px]");
         menu?.classList.add("max-h-0");
+        toggleButton.classList.remove("rotate-180");
       }
     });
 });
