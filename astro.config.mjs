2b13d9caaf3709e215f0ff8ba294887de9b2e202
victor mora rosique <vic23mora@gmail.com>
2025-02-25 02:53:55 +0100

    npm run build realizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const BASE_PATH = 'PortfolioAstro';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: `https://github.com/Vic2396/${BASE_PATH}`,
+  base: BASE_PATH,
+  output: 'static'
 });
