789bae864323fd75638dde192e0911f23da68cd5
victor mora rosique <vic23mora@gmail.com>
2025-02-25 11:18:30 +0100

    Eliminación de sección y componentes de idiomas

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-const BASE_PATH = 'PortfolioAstro';
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: `https://github.com/Vic2396/${BASE_PATH}`,
-  base: BASE_PATH,
-  output: 'static'
+  site: `https://vic2396.github.io/PortfolioAstro/`,
 });
