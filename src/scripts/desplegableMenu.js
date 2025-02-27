826861daa577ee1ba66f0d8f84a1b8cc8b216ad5
victor mora rosique <vic23mora@gmail.com>
2025-02-27 03:36:56 +0100

    refact: cierre navigation al pulsar link

--- src/scripts/desplegableMenu.js
+++ src/scripts/desplegableMenu.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.getElementById("toggle-menu");
+    const link = document.getElementsByClassName("links");
     const menu = document.getElementById("dropdown-menu");
 
     toggleButton?.addEventListener("click", () => {
@@ -12,5 +13,19 @@ document.addEventListener("DOMContentLoaded", () => {
         menu?.classList.add("max-h-0");
         toggleButton.classList.remove("rotate-180");
       }
+    });
+
+    document.querySelectorAll(".links").forEach((link) => {
+      link.addEventListener("click", () => {
+        if (menu?.classList.contains("max-h-0")) {
+          menu.classList.remove("max-h-0");
+          menu.classList.add("max-h-[200px]"); 
+          toggleButton.classList.add("rotate-180");
+        } else {
+          menu?.classList.remove("max-h-[200px]");
+          menu?.classList.add("max-h-0");
+          toggleButton.classList.remove("rotate-180");
+        }
+      });
     });
 });
